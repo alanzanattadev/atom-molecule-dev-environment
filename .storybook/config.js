3da745de9730ef2f5757b26173439d54b295e6f4
alanzanattadev <alan.zanatta@outlook.com>
2016-11-25 23:13:09 +0100

    Changes Target design

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
-import { configure } from '@kadira/storybook';
+import { configure, addDecorator } from '@kadira/storybook';
+import React from 'react';
+import {StyleRoot} from 'radium';
 
 const req = require.context('../lib/', true, /story\.js$/)
 
 function loadStories() {
   req.keys().forEach(req)
 }
+
+addDecorator((story) => (
+  <StyleRoot>
+    {story()}
+  </StyleRoot>
+));
 
 configure(loadStories, module);
