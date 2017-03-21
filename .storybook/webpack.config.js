5efca1d056392c5785a19592ad6ba50037eeeb8c
alanzanattadev <alan.zanatta@outlook.com>
2017-03-21 12:43:15 +0100

    Fixes storybook missing module

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,14 @@
+var genDefaultConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js');
+var path = require('path');
+
+module.exports = function(config, env) {
+  var config = genDefaultConfig(config, env);
+
+  return Object.assign({}, config, {
+    resolve: Object.assign({}, config.resolve, {
+      alias: {
+        remote: path.join(__dirname, 'fake/remote.js')
+      }
+    })
+  });
+};
