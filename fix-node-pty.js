29bd97cefa9a7f7d099a5e878836d935afeeb0b4
Lorenzo Alfieri <keepcalmandwazo@gmail.com>
2017-05-27 16:41:37 +0200

    Fix windows install (#213)
    * Fix windows install
    
    * Fix postinstall

---
+++ fix-node-pty.js
@@ -0,0 +1,37 @@
+"use strict";
+// @flow
+const spawn = require("child_process").spawnSync;
+
+if (process.platform === "win32") {
+  const atom_version = spawn("cmd.exe", ["/c", "atom", "-v"]);
+  const electron_version = atom_version.stdout
+    .toString()
+    .split("\n")[2]
+    .split(":")[1]
+    .replace(/\s+/g, "");
+  spawn("cmd.exe", [
+    "/c",
+    "npm",
+    "rebuild",
+    "--runtime=electron",
+    `--target=${electron_version}`,
+    "--disturl=https://atom.io/download/atom-shell",
+    "--build-from-source",
+  ]);
+} else {
+  const atom_version = spawn("bash", ["-c", "atom", "-v"]);
+  const electron_version = atom_version.stdout
+    .toString()
+    .split("\n")[2]
+    .split(":")[1]
+    .replace(/\s+/g, "");
+  spawn("bash", [
+    "-c",
+    "npm",
+    "rebuild",
+    "--runtime=electron",
+    `--target=${electron_version}`,
+    "--disturl=https://atom.io/download/atom-shell",
+    "--build-from-source",
+  ]);
+}
