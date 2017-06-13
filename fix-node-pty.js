7914917997669db231abe3f27be31748996c1dde
Lorenzo Alfieri <keepcalmandwazo@gmail.com>
2017-06-13 23:08:25 +0200

    Fix postInstall script (#225) Fixes #222

--- fix-node-pty.js
+++ fix-node-pty.js
@@ -3,7 +3,7 @@
 const spawn = require("child_process").spawnSync;
 
 if (process.platform === "win32") {
-  const atom_version = spawn("cmd.exe", ["/c", "atom", "-v"]);
+  const atom_version = spawn("cmd.exe", ["/c", "atom -v"]);
   const electron_version = atom_version.stdout
     .toString()
     .split("\n")[2]
@@ -19,7 +19,7 @@ if (process.platform === "win32") {
     "--build-from-source",
   ]);
 } else {
-  const atom_version = spawn("bash", ["-c", "atom", "-v"]);
+  const atom_version = spawn("bash", ["-c", "atom -v"]);
   const electron_version = atom_version.stdout
     .toString()
     .split("\n")[2]
